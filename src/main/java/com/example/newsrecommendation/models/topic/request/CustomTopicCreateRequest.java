cf32322ec21f8dd338523e228a15fb86b4a2dbf4
Алиев Владислав Ренатович <marknext592@gmail.com>
2025-02-16 22:56:55 +0300

    Delete src directory

--- src/main/java/com/example/newsrecommendation/models/topic/request/CustomTopicCreateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.newsrecommendation.models.topic.request;
-
-import com.example.newsrecommendation.models.user.AuthenticationCredentials;
-
-public record CustomTopicCreateRequest(String email, String password, String description) {
-    public AuthenticationCredentials getCredentials(){
-        return new AuthenticationCredentials(email, password);
-    }
-}
