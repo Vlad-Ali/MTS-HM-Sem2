822db8aed36f4d87f43bb5d030df1c7f7d2583fb
Алиев Владислав Ренатович <marknext592@gmail.com>
2025-04-08 16:39:37 +0300

    Delete src directory

--- src/main/java/com/example/newsrecommendation/model/user/AuthenticationCredentials.java
+++
@@ -1,4 +0,0 @@
-package com.example.newsrecommendation.model.user;
-
-public record AuthenticationCredentials(String email, String password) {
-}
--- src/main/java/com/example/newsrecommendation/model/website/exception/WebsiteNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.newsrecommendation.model.website.exception;
-
-public class WebsiteNotFoundException extends RuntimeException {
-    public WebsiteNotFoundException(String message) {
-        super(message);
-    }
-}
